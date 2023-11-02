8645e9204b56c51694987350eef11fb9930707c3
DanyloTrofymov <dev.danylo.trofymov@gmail.com>
2023-11-02 22:00:33 +0200

    firebase

--- todo/src/app/todoItem.ts
+++
@@ -1,8 +0,0 @@
-export class TodoItem {
-  constructor(
-    public task: string,
-    public complete: boolean = false,
-    public date: string = new Date().toLocaleDateString(),) {
-    // оголошення не потрібні
-  }
-}
